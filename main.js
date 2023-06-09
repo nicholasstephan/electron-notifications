b892e13376db9f4da96f3286d9a1ec1e7f909d3b
Yaqoub Jayousi <yaqoub.jayousi@gmail.com>
2023-06-09 19:35:13 +0300

    Fixing notifications

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
-const { app, BrowserWindow, ipcMain, Notification } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { ToastNotification } = require('electron-windows-notifications');
 const path = require('path');
+const appId = 'com.electron.electron-notifications';
 
-let win;
+let mainWindow;
 let notifications = [];
 
+// Request the single instance lock
+const isPrimary = app.requestSingleInstanceLock();
+
 const createWindow = () => {
-  win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(app.getAppPath(), 'bridge.js')
-    },
+    }
   });
-  win.loadFile('renderer.html');
+  mainWindow.loadFile('renderer.html');
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  // check if this is the only instance
+  if (isPrimary) {
+    app.setAsDefaultProtocolClient("myapp");
+    createWindow();
+  } else {
+    // We are currently running in a second instance; quit the process.
+    app.quit();
+  }
 });
 
 app.on('window-all-closed', () => {
@@ -25,17 +37,48 @@ app.on('window-all-closed', () => {
 
 
 ipcMain.handle('notify', () => {
-  let notification = new Notification({
-    title: 'Basic Notification',
-    body: 'Notification from the Main process',
-  })
+  // For demo purpose, distribute notifications into two groups.
+  const group = notifications.length % 2 ? "Group 1" : "Group 2";
+  const notifId = notifications.length;
 
-  notifications.push(notification);
-  
-  notification.on('click', () => {
-    win.webContents.send('click');
+  let notification = new ToastNotification({
+    appId: appId,
+    template: `
+      <toast activationType="protocol" launch="myapp://test?notif_id=${notifId}">
+        <header 
+          id="${group}" 
+          title="${group}" 
+          arguments="${group}" />
+        <visual>
+          <binding template="ToastGeneric">
+            <image placement="appLogoOverride" hint-crop="circle" src="C:\\Windows\\IdentityCRL\\WLive48x48.png"/>
+            <text>Notification ${notifId} title</text>
+            <text>This is notification # ${notifId}</text>
+          </binding>
+        </visual>
+      </toast>`,
+    group,
+    tag: `${notifId}`
   });
 
-  notification.show();
+  notifications.push(notification);
 
+  notification.on('activated', () => console.log('Activated!'));
+  notification.show();
+});
+
+
+app.on('second-instance', (_event, argv) => {
+  if (!mainWindow || mainWindow.isDestroyed()) {
+    return;
+  }
+
+  // Last  argument is the link which activated the app.
+  const link = argv[argv.length - 1];
+  console.log("link", link);
+  
+  if (link) {
+    const notifId = link.split("=")[1];
+    mainWindow.webContents.send('click', notifId);
+  }
 });
